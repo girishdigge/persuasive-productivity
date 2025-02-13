aa2d7f1d094ef73df2c93a9d533ecd4c81e34a45
girishdigge <girishdigge@gmail.com>
2025-02-13 16:56:37 +0530

    next international languages confiured i18n

---
+++ i18n.ts
@@ -0,0 +1,18 @@
+// import { getRequestConfig } from 'next-intl/server';
+
+// export default getRequestConfig(async ({ locale }) => ({
+//   messages: (await import(`./messages/${locale}.json`)).default,
+// }));
+
+import { getRequestConfig } from 'next-intl/server';
+
+export default getRequestConfig(async () => {
+  // Provide a static locale, fetch a user setting,
+  // read from `cookies()`, `headers()`, etc.
+  const locale = 'en';
+
+  return {
+    locale,
+    messages: (await import(`./messages/${locale}.json`)).default,
+  };
+});
